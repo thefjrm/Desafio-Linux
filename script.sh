7f32288a47705fdc0cde3b75ab74cb9b9abd8f06
Felipe Martins <57295799+thefjrm@users.noreply.github.com>
2025-02-21 19:18:05 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install httpd -y
+sudo yum upgrade -y
+sudo yum install apache2 -y
 sudo yum install git -y
-sudo git clone https://github.com/thefjrm/Desafio-Linux /var/www/html/Desafio-Linux
-sudo systemctl restart httpd
+sudo git clone https://github.com/thefjrm/validacao-formulario /var/www/html/validacao-formulario
+sudo mv /var/www/html/validacao-formulario/* /var/www/html
+sudo systemctl restart apache2
 sudo curl -x POST -d "nome=Felipe José Rodrigues Martins" https://difusaotech.com.br/lab/aws/index.php
+echo "Script executado com sucesso em $(date)" >> /var/log/setup-script.log
